fadd88c77c8fcc8f0faea9efcf900a859e1bd1a9
Asadbek <huntsman6004@gmail.com>
2024-09-05 14:26:30 +0500

    update: domain

--- AutoServis/settings/production.py
+++ AutoServis/settings/production.py
@@ -27,7 +27,7 @@ SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
 CSRF_COOKIE_SECURE = True
 CSRF_TRUSTED_ORIGINS = [
-    "https://api.avtoservis.fassco.uz",
+    "https://api.davrmotors.fassco.uz",
 ]
 
 ###################################################################
